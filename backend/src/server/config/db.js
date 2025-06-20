477d596ca55c8355a33ee2cdd265469c3297fe16
Rahul Adepu <rahuladepu002@gmail.com>
2025-06-20 21:55:27 +0530

    Protected sensitive information

--- backend/src/server/config/db.js
+++ backend/src/server/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/dunzodatabasenew');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully!');
     } catch (err) {
         console.error('MongoDB connection failed:', err.message);
